178aa17f0549e8fba3bcd1c9c2ad9b3d0d80ddcb
Oleksandr1233212 <pp99241979@gmail.com>
2024-07-03 16:12:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,257 @@
+
+const Express = require("express"); 
+const MongoClient = require("mongodb").MongoClient; 
+const cors = require("cors"); 
+const multer = require("multer"); 
+const bcrypt = require('bcrypt'); // для хешування паролів нужно установить
+
+let app = Express();
+
+app.use(cors());
+
+const { CONNECTION_STRING } = require('./config');
+
+
+
+
+let DATABASENAME = "TaskManagerApp";
+let database;
+
+const PORT = 5031;
+MongoClient.connect(CONNECTION_STRING, { useUnifiedTopology: true }).then(client => {
+    console.log('MONGO DB Connection OK!');
+    database = client.db(DATABASENAME);
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch(error => {
+    console.error('MONGO DB Connection Error:', error);
+});
+
+
+
+app.get('/api/tasknanagerapp/get', (request,response)=>{
+    
+    database.collection("TaskManagerAppCollection").find({}).toArray((error,result)=>{
+        response.send(result);
+    })
+});
+app.post('/api/tasknanagerapp/user', multer().none(),(request,response)=>{
+    const userId=request.body.userId
+    
+    database.collection("TaskManagerAppCollection").find({userId:userId}).toArray((error,result)=>{
+        response.send(result);
+    })
+       
+})
+app.get('/api/tasknanagerapp/data/:id', (request,response)=>{
+    const taskId = request.params.id;  // Отримуємо id задачі з URL
+    console.log(taskId);
+
+    // Використання ObjectId для пошуку документу за id у MongoDB
+    database.collection("TaskManagerAppCollection").findOne({id: taskId}, (error, result) => {
+        if (error) {
+            response.status(500).send("Error retrieving data from the database.");
+        } else {
+            if (result) {
+                response.send(result);
+            } else {
+                response.status(404).send("Task not found.");
+            }
+        }
+    });
+});
+
+app.post('/api/tasknanagerapp/add',multer().none(),(request,response)=>{
+    database.collection("TaskManagerAppCollection").count({}, function(error,numOfDocs){
+        database.collection("TaskManagerAppCollection").insertOne({
+            id:(numOfDocs+1).toString(),
+            name:request.body.name,
+            author:request.body.author,
+            data:request.body.createdAt,
+            category:request.body.category,
+            edited:request.body.edited,
+            completed:request.body.completed,
+            added:request.body.timeAdded,
+            timecomplited:request.body.timeComplited,
+            userId:request.body.userId
+        });
+        response.json("Added Done");
+        console.log(request.body.name)
+    })
+});
+
+app.post('/api/tasknanagerapp/del',(request,response)=>{
+    database.collection("TaskManagerAppCollection").deleteOne({
+        id:request.query.id
+    })
+        response.json("Delete Done!");
+   
+});
+
+app.post('/api/tasknanagerapp/upd', multer().none(), (request, response) => {
+    const name = request.body.name;
+    const author = request.body.author;
+    const category = request.body.category;
+    const edited = request.body.edited;
+    const completed=request.body.completed;
+    const timecomplited=request.body.timeComplited
+
+    console.log(name);
+    database.collection("TaskManagerAppCollection").findOneAndUpdate(
+        { id: request.query.id },
+        { $set: { name, author, category, edited, completed, timecomplited} },
+        { returnOriginal: false }, 
+        (error, result) => {
+            if (error) {
+                response.status(500).json("Error updating task");
+            } else {
+                response.json("Update Done!");
+            }
+        }
+    );
+});
+app.post('/api/tasknanagerapp/login', multer().none(),(request,response)=>{ 
+    const username = request.body.username; 
+    const password = request.body.password; 
+    const email = request.body.email;
+ 
+    if(!username || !password || !email) { 
+        return response.status(400).send('UserName and Password and Email are required!'); 
+    } 
+ 
+ 
+    database.collection("Users").findOne({email:email}, (error, user)=>{ 
+        if(error){ 
+            return response.status(500).send(' Error Login!');  
+        } 
+ 
+        if(!user) { 
+            return response.status(404).send('Email Is Not Found!');  
+        } 
+ 
+ 
+        
+        if(password==user.password){
+            response.status(201).json({
+                userId:user._id
+            })
+
+        }
+ 
+ 
+        if(password!=user.password) { 
+            return response.status(401).send("Invalid Password!") 
+        } 
+ 
+ 
+        
+    }) 
+    
+})
+
+app.post('/api/tasknanagerapp/register', multer().none(), (request,response)=>{
+
+    console.log(request.body);
+
+    const username = request.body.username;
+    const password = request.body.password;
+    const email=request.body.email
+
+    if(!username || !password) {
+        return response.status(400).send('UserName and Password are required!');
+    }
+    database.collection("Users").findOne({email:email}, (error, user)=>{
+        if(error){
+            return response.status(500).send(' Error Register!');  
+        }
+        console.log(user)
+ 
+        if(user) {
+            return response.status(404).send('This Email is already in use');  
+            
+        }
+
+    // const hashedPassword = bcrypt.hash(password, 10);
+    // console.log(hashedPassword)
+
+    
+
+
+    database.collection("Users").insertOne({
+        username: username,
+        password: password,
+        email:email
+    }, (error, result) => {
+        if(error){
+            return response.status(500).send('Error for register new User!'); 
+        }
+        
+    })
+    database.collection("Users").findOne({email:email}, (error, user)=>{
+        if(error){
+            response.status(500).send("Server baobab")
+    
+        }
+        if(user){
+            response.status(201).json({
+                userId:user._id
+            })
+    
+            
+        }
+    
+     })
+
+    
+    
+ })
+ 
+})
+
+
+
+
+
+
+app.post('/api/tasknanagerapp/registere', multer().none(), (request,response)=>{
+
+    console.log(request.body);
+
+    const username = request.body.username;
+    const password = request.body.password;
+    const email=request.body.email
+
+    if(!username || !password) {
+        return response.status(400).send('UserName and Password are required!');
+    }
+    database.collection("Users").findOne({email:email}, (error, user)=>{
+        if(error){
+            return response.status(500).send(' Error Register!');  
+        }
+        console.log(user)
+ 
+        if(user) {
+            return response.status(404).send('This Email is already in use');  
+            
+        }
+
+    const hashedPassword = bcrypt.hash(password, 10);
+
+    
+
+
+    database.collection("Users").insertOne({
+        username: username,
+        password: hashedPassword,
+        email:email
+    }, (error, result) => {
+        if(error){
+            return response.status(500).send('Error for register new User!'); 
+        }
+        response.status(201).send('User register succesfully!');
+    })
+    
+ })
+})
