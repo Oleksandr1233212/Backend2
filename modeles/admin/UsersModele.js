831b4359a17efa04659e61db3ce88b212f7aa1e1
Oleksandr1233212 <pp99241979@gmail.com>
2024-10-03 11:56:37 +0300

    admin

--- modeles/admin/UsersModele.js
+++ modeles/admin/UsersModele.js
@@ -14,6 +14,7 @@ const taskSchema = new mongoose.Schema({
     role:{type: String,required:true},
     password: {type:String,required:true},
     email:{type:String,required:true},
+    gender:{type:String,required:true}
     
     
 });
